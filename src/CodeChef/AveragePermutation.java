159d74af0e5920f597a6ab3eee034f0045399e13
Aryan Raj <aryanxrajj@gmail.com>
2025-02-07 10:32:53 +0530

    Learning1

--- src/CodeChef/AveragePermutation.java
+++ src/CodeChef/AveragePermutation.java
@@ -1,21 +1,34 @@
 package CodeChef;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class AveragePermutation {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         int Test_case = input.nextInt();
-
         while(Test_case-- > 0){
             int n = input.nextInt();
-
-            for(int i = 0; i<n-2;i++){
-                for(int j = 1;j<=n;j++){
-
+            List<Integer> list = new ArrayList<>();
+            if(n%2==0){
+                for(int i=1;i<=n;i++){
+                    list.add(i);
+                }
+                Collections.swap(list,n-2,0);
+            }else{
+                for(int i=n;i>=1;i--){
+                    list.add(i);
                 }
             }
+
+            for(int num: list){
+                System.out.print(num+" ");
+            }
+
+            System.out.println();
+
         }
+
+
     }
 }
