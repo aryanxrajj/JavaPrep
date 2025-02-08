3154b3b6d9ad1ce0f813361875ada761e9c2002f
Aryan Raj <aryanxrajj@gmail.com>
2025-02-08 23:16:48 +0530

    Learning1

--- src/DSA/InsertionSort.java
+++ src/DSA/InsertionSort.java
@@ -4,31 +4,10 @@ import java.util.Arrays;
 public class InsertionSort {
     public static void main(String[] args) {
         int[] nums = {5,3,4,1,2};
-        selection(nums);
         System.out.println(Arrays.toString(nums));
     }
 
-    private static int find(int[] nums,int last) {
-        int max = 0 ;
-        for(int i=0;i<=last;i++){
-            if(nums[i]>nums[max]){
-                max = i;
-            }
-        }
-        return max;
-    }
+   static void insertionsort(int[] arr){
 
-    static void selection(int[] nums){
-        for(int i = 0;i<nums.length;i++){
-            int last  = nums.length-i-1;
-            int getmax = find(nums,last);
-            swap(nums,getmax,last);
-        }
-    }
-
-    private static void swap(int[] nums,int first, int last) {
-        int temp = nums[first];
-        nums[first]= nums[last];
-        nums[last] = temp;
-    }
+   }
 }
--- src/DSA/Selectionsort.java
+++ src/DSA/Selectionsort.java
@@ -9,20 +9,27 @@ public class Selectionsort {
     }
 
     static void selection(int[] arr){
-        for(int i=0;i<arr.length-1;i++){
-            int min_index = i;
-            for(int  j =i+1;j<arr.length;j++){
-                if(arr[j]<arr[min_index]){
-                    min_index = j;
-                }
-            }
-            swap(arr,i,min_index);
+        for(int i=0;i<arr.length;i++){
+            //find the maximum element and put it at the correct index;
+            int last = arr.length-i-1;
+            int findmax =  max(arr,last);
+            swap(arr,findmax,last);
         }
     }
 
-    static void swap(int[] arr, int first, int second) {
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
+    static void swap(int[] arr, int findmax, int last) {
+        int temp = arr[findmax];
+        arr[findmax] = arr[last];
+        arr[last] = temp;
+    }
+
+    static int max(int[] arr, int last) {
+        int max_element = 0;
+        for(int i = 0;i<=last;i++){
+            if(arr[i] > arr[max_element]){
+                max_element = i;
+            }
+        }
+        return max_element;
     }
 }
