dbd4b417c5f3a5af7bf96fdc269c0f3f885b71c6
Aryan Raj <aryanxrajj@gmail.com>
2025-02-19 17:48:19 +0530

    Learning1

--- src/Assignment/leetcode/Ques605.java
+++ src/Assignment/leetcode/Ques605.java
@@ -3,19 +3,24 @@ package Assignment.leetcode;
 public class Ques605 {
     public static void main(String[] args) {
         int[] arr = {1,0,0,0,1};
-        int n = 1;
+        int n = 2;
         System.out.println(canPlaceFlowers(arr,n));
     }
 
     public static boolean canPlaceFlowers(int[] flowerbed, int n) {
-        for(int i = 0;i < flowerbed.length-1;i++){
-            if(flowerbed[i-1] == 0 && flowerbed[i+1] == 0) {
-                n--;
-            }
-        }
-
         if(n == 0) {
             return true;
+        }
+
+        for(int i = 0;i < flowerbed.length;i++){
+            if(flowerbed[i] == 0 &&(i==0 || flowerbed[i-1] == 0) && (i == flowerbed.length - 1 || flowerbed[i+1] == 0)) {
+                flowerbed[i] = 1;
+                n--;
+            }
+
+            if(n==0){
+                return true;
+            }
         }
         return false;
     }
