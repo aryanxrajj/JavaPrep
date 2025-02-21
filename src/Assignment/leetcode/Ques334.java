cb77bd0342c8398befc9c0f72cc1217442d167ef
Aryan Raj <aryanxrajj@gmail.com>
2025-02-21 23:38:51 +0530

    Learning1

---
+++ src/Assignment/leetcode/Ques334.java
@@ -0,0 +1,29 @@
+package Assignment.leetcode;
+
+public class Ques334 {
+    public static void main(String[] args) {
+        int[] arr = {20,100,10,12,5,13};
+        boolean result = increasingTriplet(arr);
+        System.out.println(result);
+    }
+
+    public static boolean increasingTriplet(int[] nums) {
+        int i = 0;
+        int j = 1;
+        int k = 2;
+
+        boolean result = false;
+
+        while(k < nums.length){
+            if(nums[i] < nums[j] && nums[i] < nums[k] && nums[j] < nums[k]){
+                result = true;
+                break;
+            }
+            i++;
+            j++;
+            k++;
+        }
+
+        return result;
+    }
+}
