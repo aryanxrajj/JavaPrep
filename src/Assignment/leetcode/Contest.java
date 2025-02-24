bdb715606bebd7c63c326c63ff91ff6582e4b968
Aryan Raj <aryanxrajj@gmail.com>
2025-02-24 09:33:51 +0530

    Learning1

---
+++ src/Assignment/leetcode/Contest.java
@@ -0,0 +1,13 @@
+package Assignment.leetcode;
+
+public class Contest {
+    public static void main(String[] args){
+
+    }
+
+    public boolean hasSameDigits(String s) {
+        int num = Integer.parseInt(s);
+        StringBuilder result = new StringBuilder();
+        return false;
+    }
+}
