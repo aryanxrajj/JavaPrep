cd15f37f749e478d8472695f37e21c3ae44ba6c5
Aryan Raj <aryanxrajj@gmail.com>
2025-02-28 15:39:43 +0530

    Learning1

---
+++ src/Misty/Practice.java
@@ -0,0 +1,9 @@
+package Misty;
+
+public class Practice {
+    public static void main(String[] args) {
+        int seconds = 3665;
+        System.out.println(seconds%60);
+        System.out.println();
+    }
+}
