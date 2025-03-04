2638f3711821c1f68241331fbb9fd2d519dd8107
Aryan Raj <aryanxrajj@gmail.com>
2025-03-04 22:12:25 +0530

    Learning1

---
+++ src/Assignment/GFG/ValidParenthesis.java
@@ -0,0 +1,36 @@
+package Assignment.GFG;
+import java.util.*;
+
+public class ValidParenthesis {
+    public static void main(String[] args) {
+        String s = "[{()}]";
+    }
+
+    public static boolean isBalanced(String s){
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : s.toCharArray()){
+            if(ch == '[' || ch == '(' || ch == '{'){
+                stack.push(ch);
+            }else{
+                if(ch == ')'){
+                    if(stack.isEmpty() || stack.pop()!='(') {
+                        return false;
+                    }
+                }
+                if(ch == ']'){
+                    if(stack.isEmpty() || stack.pop()!='[') {
+                        return false;
+                    }
+                }
+                if(ch == '}'){
+                    if(stack.isEmpty() || stack.pop()!='{') {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
