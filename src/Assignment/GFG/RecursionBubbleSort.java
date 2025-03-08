345baabf87bbfd72c619b5d6863d3d41ede914fc
Aryan Raj <aryanxrajj@gmail.com>
2025-03-08 20:02:49 +0530

    Learning1

--- src/Assignment/GFG/RecursionBubbleSort.java
+++ src/Assignment/GFG/RecursionBubbleSort.java
@@ -1,11 +1,15 @@
 package Assignment.GFG;
 
+import java.util.Arrays;
+
 public class RecursionBubbleSort {
     public static void main(String[] args) {
-
+        int[] arr = {5,4,3,2,1};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
-    public static void bubbleSort(int arr[]) {
+    public static void bubbleSort(int[] arr) {
 
     }
 }
