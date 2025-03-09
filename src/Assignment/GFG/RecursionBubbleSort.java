83d078ff1b82c9d9649d9e67e1ee176dc59c1ecf
Aryan Raj <aryanxrajj@gmail.com>
2025-03-09 23:01:00 +0530

    Learning1

--- src/Assignment/GFG/RecursionBubbleSort.java
+++ src/Assignment/GFG/RecursionBubbleSort.java
@@ -5,11 +5,13 @@ import java.util.Arrays;
 public class RecursionBubbleSort {
     public static void main(String[] args) {
         int[] arr = {5,4,3,2,1};
-        bubbleSort(arr);
+        bubbleSort(arr,arr.length);
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void bubbleSort(int[] arr) {
-
+    public static void bubbleSort(int[] arr,int n) {
+        if(n == 1){
+            return;
+        }
     }
 }
