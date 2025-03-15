263c852c16b17ea5503e5d2a4b4d53c111460c0a
Aryan Raj <aryanxrajj@gmail.com>
2025-03-15 23:12:01 +0530

    Learning1

---
+++ src/Assignment/leetcode/Ques43.java
@@ -0,0 +1,30 @@
+package Assignment.leetcode;
+
+import java.math.BigInteger;
+
+public class Ques43 {
+    public static void main(String[] args) {
+        String num1 = "123";
+        String num2 = "456";
+
+        System.out.println(multiply(num1,num2));
+    }
+
+    public static  String multiply(String num1, String num2) {
+        int first = 0;
+        int second = 0;
+
+        for(char ch : num1.toCharArray()){
+            first = first * 10 + ch - '0';
+        }
+
+        for(char ch : num2.toCharArray()){
+            second = second * 10 + ch -'0';
+        }
+
+        BigInteger a = new BigInteger(String.valueOf(first));
+        BigInteger b = new BigInteger(String.valueOf(second));
+
+        return String.valueOf(a.multiply(b));
+    }
+}
--- src/DSA/BST.java
+++ src/DSA/BST.java
@@ -3,99 +3,66 @@ package DSA;
 import java.util.Scanner;
 
 class Node {
-    char data;
+
+    int  value;
     Node left, right;
 
-    public Node(char data) {
-        this.data = data;
+    public Node(){}
+
+    public Node(int value) {
+        this.value = value;
         this.left = this.right = null;
+    }
+
+    public Node(int value, Node left, Node right){
+        this.value = value;
+        this.left = left;
+        this.right = right;
     }
 }
 
 public class BST{
+    Node root;
 
-    // Function to insert a node into the BST
-    public static Node insert(Node root, char data) {
-        if (root == null) {
-            return new Node(data);
+    public void insert(int key){
+        root = insertRec(root,key);
+    }
+
+    public Node insertRec(Node root, int key){
+        if(root == null){
+            root = new Node(key);
+            return root;
         }
-        if (data <= root.data) {
-            root.left = insert(root.left, data);
-        } else {
-            root.right = insert(root.right, data);
+
+        if(key < root.value){
+            root.left = insertRec(root.left,key);
+        }else if(key > root.value){
+            root.right = insertRec(root.right,key);
         }
+
         return root;
     }
 
-    // Function to find the node with the minimum value in the BST
-    public static Node findMin(Node root) {
-        while (root.left != null) {
-            root = root.left;
-        }
-        return root;
+    public void inorder(){
+        inorderRec(root);
     }
 
-    // Function to delete a node from the BST
-    public static Node deleteNode(Node root, char data) {
-        if (root == null) {
-            return null;
-        }
-
-        if (data < root.data) {
-            root.left = deleteNode(root.left, data); // Search in the left subtree
-        } else if (data > root.data) {
-            root.right = deleteNode(root.right, data); // Search in the right subtree
-        } else {
-            // Node to be deleted found
-            if (root.left == null && root.right == null) {
-                // Case 1: Leaf node
-                return null;
-            } else if (root.left == null) {
-                // Case 2: Node with only right child
-                return root.right;
-            } else if (root.right == null) {
-                // Case 3: Node with only left child
-                return root.left;
-            } else {
-                // Case 4: Node with two children
-                Node temp = findMin(root.right); // Find the minimum value in the right subtree
-                root.data = temp.data; // Copy the value to the current node
-                root.right = deleteNode(root.right, temp.data); // Delete the duplicate node
-            }
-        }
-        return root;
-    }
-
-    // Function to perform inorder traversal of the BST
-    public static void inorder(Node root) {
-        if (root != null) {
-            inorder(root.left);
-            System.out.print(root.data + " ");
-            inorder(root.right);
+    public void inorderRec(Node root){
+        if(root != null){
+            inorderRec(root.left);
+            System.out.print(root.value + "->");
+            inorderRec(root.right);
         }
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        Node root = null;
+        BST bst = new BST();
+        bst.insert(10);
+        bst.insert(20);
+        bst.insert(30);
+        bst.insert(40);
+        bst.insert(50);
 
-        // Insert nodes into the BST
-        for (int i = 0; i < size; i++) {
-            char input = sc.next().charAt(0);
-            root = insert(root, input);
-        }
-
-        // Read the character to be deleted
-        char charToDelete = sc.next().charAt(0);
-
-        // Delete the specified node from the BST
-        root = deleteNode(root, charToDelete);
-
-        // Perform inorder traversal to display the BST
-        inorder(root);
-        System.out.println();
-
-        sc.close();
+        bst.inorder();
     }
 }
