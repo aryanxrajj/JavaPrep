7a5e0a20a85227cc31980f7a6c93e8f64c0a7010
Aryan Raj <aryanxrajj@gmail.com>
2025-03-29 16:06:03 +0530

    Learning1

--- src/LambdaExpression/Introduction.java
+++ src/LambdaExpression/Introduction.java
@@ -1,7 +1,25 @@
 package LambdaExpression;
 
+import java.util.function.Predicate;
+
+//Functional Interface
+interface MyNumber{
+    double getValue();
+}
+
+interface MyNumber1{
+    boolean isEven(int n);
+}
+
 public class Introduction {
     public static void main(String[] args) {
-
+        MyNumber mynum;
+        mynum = () -> 123.45;
+        System.out.println(mynum.getValue());
+        MyNumber1 mynum1;
+        mynum1 = (n) -> n % 2 == 0;
+        System.out.println(mynum1.isEven(10));
+        Predicate<Integer> result = (n) -> n % 2 == 0;
+        System.out.println(result.test(5));
     }
 }
