e6ebf662aa161738ac224b4e86c47f3d3a780726
Aryan Raj <aryanxrajj@gmail.com>
2025-04-03 22:53:53 +0530

    Learning1

---
+++ src/Assignment/leetcode/Ques125.java
@@ -0,0 +1,23 @@
+package Assignment.leetcode;
+
+public class Ques125 {
+    public static void main(String[] args) {
+        String s = "race a car";
+        System.out.println(isPalindrome(s));
+    }
+
+    public static boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        StringBuilder str = new StringBuilder();
+        for(int i = 0; i < s.length();i++){
+            char ch = s.charAt(i);
+            if(Character.isLetter(ch)){
+                str.append(ch);
+            }
+        }
+        StringBuilder rev = new StringBuilder(str);
+        rev.reverse();
+
+        return str.toString().equals(rev.toString());
+    }
+}
