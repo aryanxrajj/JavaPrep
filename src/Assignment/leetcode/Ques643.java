e92ddfb80df3b0dd6f201961872f69d56d7af9cd
Aryan Raj <aryanxrajj@gmail.com>
2025-04-08 20:52:55 +0530

    Learning1

---
+++ src/Assignment/leetcode/Ques643.java
@@ -0,0 +1,18 @@
+package Assignment.leetcode;
+
+public class Ques643 {
+    public static void main(String[] args) {
+        int[] nums = {1,12,-5,-6,50,3};
+    }
+
+    public static double findMaxAverage(int[] nums, int k) {
+        int max = Integer.MIN_VALUE;
+        int start = 0 , end = 0;
+
+        while(end < nums.length){
+
+        }
+
+        return 0.0;
+    }
+}
