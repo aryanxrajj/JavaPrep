ca87f5ced384dc22ac2c21ac98a92341e09997c3
Aryan Raj <aryanxrajj@gmail.com>
2025-04-19 23:14:11 +0530

    Learning1

---
+++ src/Misty/TortoiseAndHareAlgo.java
@@ -0,0 +1,16 @@
+package Misty;
+
+import java.util.*;
+
+class ListNode{
+    int val;
+    ListNode next;
+
+    ListNode(int val){
+        this.val = val;
+    }
+}
+
+public class TortoiseAndHareAlgo{
+
+}
