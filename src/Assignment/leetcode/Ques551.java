9fd955261c3266a7a58606d72a755c03ce881a7f
Aryan Raj <aryanxrajj@gmail.com>
2025-04-28 10:39:10 +0530

    Learning1

---
+++ src/Assignment/leetcode/Ques551.java
@@ -0,0 +1,37 @@
+package Assignment.leetcode;
+
+public class Ques551 {
+    public static void main(String[] args) {
+        String s  = "PPALLP";
+        System.out.println(checkRecord(s));
+
+        String s1 = "PPALLL";
+        System.out.println(checkRecord(s1));
+    }
+
+    public static boolean checkRecord(String s) {
+        int aCount = 0, lCount = 0, pCount = 0;
+
+        for(int i = 0;i < s.length(); i++){
+            if(s.charAt(i) == 'A'){
+                aCount++;
+                lCount = 0;
+            }
+
+            if(s.charAt(i) == 'P'){
+                pCount++;
+                lCount = 0;
+            }
+
+            if(s.charAt(i) == 'L'){
+                lCount++;
+            }
+
+            if(lCount == 3){
+                return false;
+            }
+        }
+
+        return aCount < 2;
+    }
+}
