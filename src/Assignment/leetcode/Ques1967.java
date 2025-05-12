ea426f5241de43c8d77d3d7dab6da4b4ecc15fcb
Aryan Raj <aryanxrajj@gmail.com>
2025-05-12 22:20:01 +0530

    Learning1

---
+++ src/Assignment/leetcode/Ques1967.java
@@ -0,0 +1,20 @@
+package Assignment.leetcode;
+
+public class Ques1967 {
+    public static void main(String[] args){
+        String[] arr = {"a","abc","bc","d"};
+        String word = "abc";
+        System.out.println(numOfStrings(arr,word));
+    }
+
+    public static int numOfStrings(String[] patterns, String word) {
+        int count = 0;
+        for(String s : patterns){
+            if(word.contains(s)){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
