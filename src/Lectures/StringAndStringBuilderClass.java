34eedfaf03e0a5dc35ea94b787a3a1f14a4939c8
Aryan Raj <aryanxrajj@gmail.com>
2025-05-28 09:07:05 +0530

    Learning1

--- src/Lectures/StringAndStringBuilderClass.java
+++ src/Lectures/StringAndStringBuilderClass.java
@@ -2,13 +2,9 @@ package Lectures;
 
 public class StringAndStringBuilderClass {
     public static void main(String[] args) {
-        String name = new String("Hello, World");
-        System.out.println(name);
-
-        String name2 = "Aryan Raj";
-        System.out.println(name2);
-
-//        String name3 = new String(byte[] bytes);
+        StringBuilder sb = new StringBuilder("Hello");
+        StringBuilder sb2 = new StringBuilder("Hello");
+        System.out.println(sb.compareTo(sb2) == 0);
 
     }
 }
