5962c1f8560c4011d15e80520181a3f946ab6ffc
Aryan Raj <aryanxrajj@gmail.com>
2025-05-28 09:06:56 +0530

    Learning1

---
+++ src/Assignment/leetcode/Ques2465.java
@@ -0,0 +1,30 @@
+package Assignment.leetcode;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Ques2465 {
+    public static void main(String[] args) {
+        int[] nums = {4,1,4,0,3,5};
+        System.out.println(distinctAverages(nums));
+
+        int[] nums2 = {1,100};
+        System.out.println(distinctAverages(nums2));
+    }
+
+    public static int distinctAverages(int[] nums) {
+        Arrays.sort(nums);
+        int start = 0, end = nums.length - 1;
+        Set<Double> set = new HashSet<>();
+
+        while(start < end){
+            double average = (double)(nums[start]+nums[end])/2;
+            set.add(average);
+            start++;
+            end--;
+        }
+
+        return set.size();
+    }
+}
