204d1523e7ad0033ba4df86d22b9b7137ec79608
Aryan Raj <aryanxrajj@gmail.com>
2025-05-29 16:57:51 +0530

    Learning1

---
+++ src/Assignment/leetcode/Ques2678.java
@@ -0,0 +1,20 @@
+package Assignment.leetcode;
+
+public class Ques2678 {
+    public static void main(String[] args) {
+        String[] details = {"7868190130M7522","5303914400F9211","9273338290F4010"};
+        System.out.println(countSeniors(details));
+    }
+
+    public static int countSeniors(String[] details) {
+        int count  = 0;
+        for(String s : details){
+            String sub = s.substring(11,13);
+            if(Integer.parseInt(sub) > 60){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
