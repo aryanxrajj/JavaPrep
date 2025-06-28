a39c3249dcba505f5a4f838f5a7e16f91de938b5
Aryan Raj <aryanxrajj@gmail.com>
2025-06-28 23:34:55 +0530

    Update Ques21

--- src/Assignment/leetcode/Ques21.java
+++ src/Assignment/leetcode/Ques21.java
@@ -1,86 +1,53 @@
 package Assignment.leetcode;
 
+import java.util.List;
+
 public class Ques21 {
-
-    private Node head;
-    private Node tail;
-    private int size;
-
-    public Ques21() {
-        this.size = 0;
-    }
-
-    public static Ques21 mergeTwoLists(Ques21 first, Ques21 second) {
-        Node f = first.head;
-        Node s = second.head;
-        Ques21 third = new Ques21();
-        while(f != null && s != null){
-            if(f.val<s.val){
-                third.insert(f.val);
-                f = f.next;
-            }else{
-                third.insert(s.val);
-                s = s.next;
-            }
-        }
-
-        while(f != null){
-            third.insert(f.val);
-            f = f.next;
-        }
-
-        while(s != null){
-            third.insert(s.val);
-            s = s.next;
-        }
-
-        return third;
-
-    }
-        public void display() {
-            Node temp = head;
-            while (temp != null) {
-                System.out.print(temp.val + " -> ");
-                temp = temp.next;
-            }
-            System.out.println("END");
-        }
-
-    public void insert(int val){
-        Node node = new Node(val);
-        if(head==null){
-            head = node;
-            tail = node;
-        }else{
-            tail.next = node;
-            tail = node;
-        }
-    }
-
-    public class Node{
-        int val;
-        Node next;
-
-        public Node(int val) {
-            this.val = val;
-        }
-
-        public Node(int val, Node next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
-
     public static void main(String[] args) {
-        Ques21 first = new Ques21();
-        Ques21 second = new Ques21();
-        first.insert(1);
-        first.insert(2);
-        first.insert(4);
-        second.insert(1);
-        second.insert(3);
-        second.insert(4);
-        Ques21 ans  = mergeTwoLists(first,second);
-        ans.display();
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        ListNode result = mergeTwoLists(list1,list2);
+        while(result != null){
+            System.out.println(result.val + " ");
+            result = result.next;
+        }
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode result= new ListNode();
+        ListNode third = result;
+
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val){
+                third.next = list1;
+                list1 = list1.next;
+                third = third.next;
+            }else{
+                third.next = list2;
+                list2 = list2.next;
+                third = third.next;
+            }
+        }
+
+        while(list1 != null){
+            third.next = list1;
+            list1 = list1.next;
+            third = third.next;
+        }
+
+        while(list2 != null){
+            third.next = list2;
+            list2 = list2.next;
+            third = third.next;
+        }
+
+        return result.next;
     }
 }
+
