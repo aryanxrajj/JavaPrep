d73b30724a9e91f7aa98d9fc4c31d6a58dc6fb16
Aryan Raj <aryanxrajj@gmail.com>
2025-07-02 23:59:04 +0530

    CodeChef

--- src/CodeChef/BalancedAndUniqueArray.java
+++ src/CodeChef/BalancedAndUniqueArray.java
@@ -6,60 +6,48 @@ import java.util.Scanner;
 public class BalancedAndUniqueArray {
     public static void main (String[] args) throws java.lang.Exception
     {
-        Scanner input = new Scanner(System.in);
-        int test_cases = input.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
 
-        while(test_cases-- > 0){
-            int n = input.nextInt();
-            int[] arr = new int[n];
-            ArrayList<Integer> arr2 = new ArrayList<>();
-            ArrayList<Integer> arr3 = new ArrayList<>();
-            int sum1 = 0;
-            int sum2 = 0;
+        for (int k = 0; k < test; k++) {
+            int num = sc.nextInt();
 
-            if(n==2 || n==0 || n%2!=0){
-                System.out.println("NO");
-            }else{
-                for(int i = 0;i < arr.length;i++){
-                    arr[i] = i+1;
-                }
-
-                for(int i = 0; i < n/2;i++){
-                    if(i%2==0){
-                        sum1+=arr[i];
-                        arr2.add(arr[i]);
-                    }else{
-                        sum2+=arr[i];
-                        arr3.add(arr[i]);
-                    }
-                }
-
-                for(int i = n/2; i < arr.length;i++){
-                    if(i%2==0){
-                        sum2+=arr[i];
-                        arr3.add(arr[i]);
-                    }else{
-                        sum1+=arr[i];
-                        arr2.add(arr[i]);
-                    }
-                }
-
-                if(sum1 == sum2){
-                    System.out.println("YES");
-                    for(int i = 0;i < arr2.size();i++){
-                        System.out.print(arr2.get(i));
-                    }
-                    System.out.println();
-                    for(int i = 0;i < arr3.size();i++){
-                        System.out.print(arr3.get(i));
-                    }
-
-                }else{
-                    System.out.println("NO");
-                }
-
+            if (num % 4 != 0) {
+                System.out.println("No");
+                continue;
             }
+
+            System.out.println("YES");
+
+            int prin = 1;
+            for (int i = 0; i < num / 4; i++) {
+                System.out.print(prin + " ");
+                prin += 2;
+            }
+
+
+            prin++;
+            for (int i = 0; i < num / 4; i++) {
+                System.out.print(prin + " ");
+                prin += 2;
+            }
+
+            System.out.println();
+
+            prin = 2;
+            for (int i = 0; i < num / 4; i++) {
+                System.out.print(prin + " ");
+                prin += 2;
+            }
+
+            prin--;
+            for (int i = 0; i < num / 4; i++) {
+                System.out.print(prin + " ");
+                prin += 2;
+            }
+
         }
+        sc.close();
 
     }
 }
