8b2af9eff3871824bde3068ab1e0c37bee5c09cd
Aryan Raj <aryanxrajj@gmail.com>
2025-07-03 22:33:12 +0530

    Binary Tree Traversal

---
+++ src/DSA/BinaryTree.java
@@ -0,0 +1,62 @@
+package DSA;
+
+
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class BinaryTree {
+
+    public static void preorder(TreeNode root){
+        if(root == null){
+            return;
+        }
+
+        System.out.print(root.val + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void inorder(TreeNode root){
+        if(root == null){
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.val + " ");
+        inorder(root.right);
+    }
+
+    public static void postorder(TreeNode root){
+        if(root == null){
+            return;
+        }
+
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.val + " ");
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        preorder(root);
+        System.out.println();
+        inorder(root);
+        System.out.println();
+        postorder(root);
+    }
+}
