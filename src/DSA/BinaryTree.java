275071fe9cd381494e8648c2c378312d068a8740
Aryan Raj <aryanxrajj@gmail.com>
2025-07-07 22:39:58 +0530

    Binary Tree Traversal

--- src/DSA/BinaryTree.java
+++ src/DSA/BinaryTree.java
@@ -50,7 +50,7 @@ public class BinaryTree {
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
-        root.left.left = new TreeNode(4);
+        root.right.left = new TreeNode(4);
         root.left.right = new TreeNode(5);
 
         preorder(root);
