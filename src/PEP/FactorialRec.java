1a104cf852a81cf80760b03cb8d5a0e4868337ec
Aryan Raj <aryanxrajj@gmail.com>
2025-07-11 21:47:08 +0530

    Binary Tree Traversal

--- src/PEP/FactorialRec.java
+++ src/PEP/FactorialRec.java
@@ -3,6 +3,7 @@ package PEP;
 public class FactorialRec {
     public static void main(String[] args) {
         System.out.println(factorial2(5));
+        System.out.println(fact(5));
     }
 
     //Factorial of a Number
@@ -22,7 +23,17 @@ public class FactorialRec {
         }
 
         return fact;
+    }
 
+    public static int fact(int n){
+        if(n == 1){
+            return n;
+        }
 
+        int fact = 1;
+        fact*=n;
+        int ans = fact(n - 1);
+        fact*=ans;
+        return fact;
     }
 }
