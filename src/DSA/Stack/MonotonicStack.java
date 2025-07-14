b87346f1224816ef5bd63ed1a215dc3c4699682a
Aryan Raj <aryanxrajj@gmail.com>
2025-07-14 13:40:41 +0530

    MonotonicStack Finding NGE And NSE

--- src/DSA/Stack/MonotonicStack.java
+++ src/DSA/Stack/MonotonicStack.java
@@ -4,21 +4,44 @@ import java.util.Stack;
 
 public class MonotonicStack {
     public static void main(String[] args) {
-        int[] nums = {73,74,75,71,69,72,76,73};
-        int[] res = nextGreaterElement(nums);
-        System.out.println(Arrays.toString(res));
+        int[] nums = {73, 74, 75, 71, 69, 72, 76};
+        System.out.println(Arrays.toString(NGE(nums)));
+        System.out.println(Arrays.toString(NSE(nums)));
     }
 
-    private static int[] nextGreaterElement(int[] temps) {
-        int[] results = new int[temps.length];
+    //NSG Greater Element
+    public static int[] NGE(int[] arr){
+        int[] result = new int[arr.length];
         Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < temps.length; i++) {
-            while (!stack.isEmpty() && temps[stack.peek()] < temps[i]) {
-                results[stack.peek()] = i - stack.pop();
+        for(int i = arr.length - 1; i >= 0; i--){
+
+            while(!stack.isEmpty() && stack.peek() <= arr[i]){
+                stack.pop();
             }
-            stack.push(i);
+
+            result[i] = stack.isEmpty() ? -1 : stack.peek();
+
+            stack.push(arr[i]);
         }
 
-        return results;
+        return result;
+    }
+
+    //Next Smaller Element
+    public static int[] NSE(int[] arr){
+        int[] result = new int[arr.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = arr.length - 1; i >=0 ; i--){
+            while(!stack.isEmpty() && stack.peek() >= arr[i]){
+                stack.pop();
+            }
+
+            result[i] = stack.isEmpty() ? -1 : stack.peek();
+
+            stack.push(arr[i]);
+        }
+
+        return result;
     }
 }
