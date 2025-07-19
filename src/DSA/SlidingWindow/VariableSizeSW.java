6417c5b866c952c49dd387dfa784df291f49f6a3
Aryan Raj <aryanxrajj@gmail.com>
2025-07-19 22:45:02 +0530

    VariableSizeSW

--- src/DSA/SlidingWindow/VariableSizeSW.java
+++ src/DSA/SlidingWindow/VariableSizeSW.java
@@ -1,39 +1,25 @@
 package DSA.SlidingWindow;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class VariableSizeSW {
-    //Geeks For Geeks
-    public static int maxLen(int[] arr) {
-        Map<Integer, Integer> prefixMap = new HashMap<>();
-        int maxLen = 0, prefixSum = 0;
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 1, 2, 4, 3};
+        int target = 7;
+        System.out.println(variableSizeWindow(arr,target));
+    }
 
-        for (int i = 0; i < arr.length; i++) {
-            prefixSum += arr[i];
+    public static int variableSizeWindow(int[] arr, int target){
+        int start = 0 , minLen = Integer.MAX_VALUE, sum = 0;
 
-            if (prefixSum == 0) {
-                maxLen = i + 1; // sum from 0 to i is 0
-            }
+        for(int end = 0; end < arr.length; end++){
+            sum+=arr[end];
 
-            if (prefixMap.containsKey(prefixSum)) {
-                int prevIndex = prefixMap.get(prefixSum);
-                maxLen = Math.max(maxLen, i - prevIndex);
-            } else {
-                prefixMap.put(prefixSum, i); // store only first occurrence
+            while(sum >= target){
+                minLen = Math.min(minLen,end - start + 1);
+                sum-=arr[start++];
             }
         }
 
-        return maxLen;
-    }
-
-    public static void main(String[] args) {
-        int[] arr1 = {15, -2, 2, -8, 1, 7, 10, 23};
-        int[] arr2 = {2, 10, 4};
-        int[] arr3 = {1, 0, -4, 3, 1, 0};
-
-        System.out.println(maxLen(arr1)); // Output: 5
-        System.out.println(maxLen(arr2)); // Output: 0
-        System.out.println(maxLen(arr3)); // Output: 5
+        return minLen;
     }
 }
