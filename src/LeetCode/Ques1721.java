bd6f50b5a57fb492ed5cee69c0b983d7907a290e
Aryan Raj <aryanxrajj@gmail.com>
2025-07-20 23:16:23 +0530

    Linked List Questions

--- src/LeetCode/Ques1721.java
+++ src/LeetCode/Ques1721.java
@@ -5,8 +5,8 @@ public class Ques1721 {
         ListNode head = new ListNode(1);
         head.next = new ListNode(2);
         head.next.next = new ListNode(3);
-//        head.next.next.next = new ListNode(4);
-//        head.next.next.next.next = new ListNode(5);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
 
         int k = 2;
         ListNode ans = swapNodes(head,k);
