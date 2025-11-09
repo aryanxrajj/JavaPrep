80a41a27edc9ce1c7354029a2c6ef3674be4f917
Aryan Raj <aryanxrajj@gmail.com>
2025-11-09 23:31:53 +0530

    count operations

---
+++ src/LeetCode/Ques2169.java
@@ -0,0 +1,24 @@
+package LeetCode;
+
+public class Ques2169 {
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 10;
+        System.out.println(countOperations(num1,num2));
+    }
+
+    public static int countOperations(int num1, int num2) {
+        int count = 0;
+
+        while(num1 != 0 && num2 != 0){
+            if(num1 >= num2){
+                num1 -= num2;
+            }else{
+                num2 -= num1;
+            }
+
+            count++;
+        }
+
+        return count;
+    }
+}
