956f9491c5ce458eafc31dc997a119eeedbe0efd
Aryan Raj <aryanxrajj@gmail.com>
2025-11-12 23:23:11 +0530

    BinaryConversion

--- src/DSA/BitManipulation/BinaryConversion.java
+++ src/DSA/BitManipulation/BinaryConversion.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class BinaryConversion {
     public static void main(String[] args) {
-        int num = 4;
+        int num = 2;
         binaryConversion(num);
     }
 
